f4a4201fb90f8e765c47b777faaf3917c43b6fba
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-06-16 16:02:30 +0200

    make the calculation more intuitive

--- dist/fractal.js
+++ dist/fractal.js
@@ -1,6 +1,9 @@
 "use strict";
 // Constants
-const SCALE = 0.0015;
+const X_MIN = -1.5;
+const X_MAX = 1.5;
+const Y_MIN = -0.95;
+const Y_MAX = 0.99;
 const MAX_ITERS = 250;
 const juliaX = -0.62580000000000;
 const juliaY = 0.40250000000000;
@@ -17,8 +20,12 @@ if (!ctx) {
     alert("Error: failed to get canvas context!");
     throw new Error("Error: failed to get canvas context!");
 }
-canvas.width = window.innerWidth - 40;
+canvas.width = window.innerWidth - 50;
 canvas.height = window.innerHeight - 200;
+const dx = (X_MAX - X_MIN) / canvas.width;
+const dy = (Y_MAX - Y_MIN) / canvas.height;
+console.log("dx: " + dx);
+console.log("dy: " + dy);
 // The HTMLElement.focus() method sets focus on the specified element, if it can be focused.
 // The focused element is the element that will receive keyboard and similar events by default.
 canvas.focus();
@@ -45,12 +52,12 @@ function drawFractal() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#FF0000";
     let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    let y = 0 - canvas.height / 2 * SCALE;
+    let y = Y_MIN;
     let p = 0;
     for (let i = canvas.height - 1; i >= 0; i--) {
-        let x = 0 - canvas.width / 2.0 * SCALE;
+        let x = X_MIN;
         for (let j = 0; j < canvas.width; j++) {
-            const iters = getNumIters(x, y, juliaX, juliaY, MAX_ITERS);
+            const iters = getNumIters(x, y, juliaX, juliaY, MAX_ITERS); // make faster
             if (iters < MAX_ITERS) {
                 imageData.data[p++] = 256;
                 imageData.data[p++] = 256;
@@ -62,9 +69,9 @@ function drawFractal() {
                 imageData.data[p++] = 0;
             }
             imageData.data[p++] = 255;
-            x += SCALE;
+            x += dx;
         }
-        y += SCALE;
+        y += dy;
     }
     ctx.putImageData(imageData, 0, 0);
 }
--- src/fractal.ts
+++ src/fractal.ts
@@ -1,10 +1,12 @@
 // Constants
-const SCALE = 0.0015;
+const X_MIN = -1.5;
+const X_MAX = 1.5;
+const Y_MIN = -0.95;
+const Y_MAX = 0.99;
+
 const MAX_ITERS = 250;
 const juliaX = -0.62580000000000;
 const juliaY = 0.40250000000000;
-
-
 
 // get the canvas element and its context
 let canvas: HTMLCanvasElement | null = document.getElementById("myCanvas") as HTMLCanvasElement;
@@ -22,8 +24,14 @@ if (!ctx) {
     throw new Error("Error: failed to get canvas context!");
 }
 
-canvas.width = window.innerWidth - 40;
+canvas.width = window.innerWidth - 50;
 canvas.height = window.innerHeight - 200;
+
+const dx = (X_MAX - X_MIN) / canvas.width;
+const dy = (Y_MAX - Y_MIN) / canvas.height; 
+
+console.log("dx: " + dx);
+console.log("dy: " + dy);
 
 // The HTMLElement.focus() method sets focus on the specified element, if it can be focused.
 // The focused element is the element that will receive keyboard and similar events by default.
@@ -40,9 +48,7 @@ function getNumIters(re: number, im: number, c_re: number, c_im: number, maxIter
         im = 2.0 * x * im;
         re += c_re;
         im += c_im;
-
     }
-
     return iters;
 }
 
@@ -58,13 +64,13 @@ function drawFractal() {
     ctx!.fillStyle = "#FF0000";
 
     let imageData: ImageData = ctx!.getImageData(0, 0, canvas!.width, canvas!.height,);
-    let y = 0 - canvas!.height / 2 * SCALE;
+    let y = Y_MIN;
 
     let p = 0;
     for (let i = canvas!.height - 1; i >= 0; i--) {
-        let x = 0 - canvas!.width / 2.0 * SCALE;
+        let x = X_MIN;
         for (let j = 0; j < canvas!.width; j++) {
-            const iters = getNumIters(x, y, juliaX, juliaY, MAX_ITERS);
+            const iters = getNumIters(x, y, juliaX, juliaY, MAX_ITERS); // make faster
             if (iters < MAX_ITERS) {
                 imageData.data[p++] = 256;
                 imageData.data[p++] = 256;
@@ -76,9 +82,9 @@ function drawFractal() {
                 imageData.data[p++] = 0;
             }
             imageData.data[p++] = 255;
-            x += SCALE;
+            x += dx;
         }
-        y += SCALE;
+        y += dy;
     }
 
     ctx!.putImageData(imageData, 0, 0);
