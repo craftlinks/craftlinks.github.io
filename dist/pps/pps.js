c85506483460cc8d1c92364b0cd4dfb08bbb09ed
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-06-29 14:25:32 +0200

    casing of folders in git

---
+++ dist/pps/pps.js
@@ -0,0 +1,212 @@
+import { createShaderModule, render } from "../lib.js";
+import GUI from '../lil-gui.esm.js';
+/////////////////////////////////////////////////////////
+// GPU and CPU Settings
+// Sizes in bytes
+const sizes = {
+    f32: 4,
+    u32: 4,
+    i32: 4,
+    vec2: 8,
+    vec4: 16,
+};
+const uniforms = {
+    rez: 1024,
+    time: 0,
+    alpha: -100,
+    beta: -3,
+    radius: 26.88,
+    count: 12666,
+};
+// CPU-only settings
+const settings = {
+    scale: (0.95 * Math.min(window.innerHeight, window.innerWidth)) / uniforms.rez,
+    pixelWorkgroups: Math.ceil(uniforms.rez ** 2 / 256),
+    agentWorkgroups: Math.ceil(uniforms.count / 256),
+};
+/////////////////////////////////////////////////////////
+// Main
+async function main() {
+    ///////////////////////
+    // Initial setup
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+        alert("No GPU found");
+        return;
+    }
+    const gpu = await adapter.requestDevice();
+    const canvas = document.createElement("canvas");
+    canvas.width = canvas.height = uniforms.rez * settings.scale;
+    document.body.appendChild(canvas);
+    const context = canvas.getContext("webgpu");
+    if (!context) {
+        alert("No WebGPU found");
+        return;
+    }
+    const format = "bgra8unorm";
+    context.configure({
+        device: gpu,
+        format: format,
+        alphaMode: "premultiplied",
+    });
+    /////////////////////////
+    // Set up memory resources
+    const visibility = GPUShaderStage.COMPUTE;
+    // Pixel buffer
+    const pixelBuffer = gpu.createBuffer({
+        size: uniforms.rez ** 2 * sizes.vec4,
+        usage: GPUBufferUsage.STORAGE,
+    });
+    const pixelBufferLayout = gpu.createBindGroupLayout({
+        entries: [{ visibility, binding: 0, buffer: { type: "storage" } }],
+    });
+    const pixelBufferBindGroup = gpu.createBindGroup({
+        layout: pixelBufferLayout,
+        entries: [{ binding: 0, resource: { buffer: pixelBuffer } }],
+    });
+    // Uniform buffers
+    const rezBuffer = gpu.createBuffer({
+        size: sizes.f32,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM,
+    });
+    const timeBuffer = gpu.createBuffer({
+        size: sizes.f32,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM,
+    });
+    gpu.queue.writeBuffer(timeBuffer, 0, new Float32Array([uniforms.time]));
+    const countBuffer = gpu.createBuffer({
+        size: sizes.f32,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM,
+    });
+    const alphaBuffer = gpu.createBuffer({
+        size: sizes.f32,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM,
+    });
+    const betaBuffer = gpu.createBuffer({
+        size: sizes.f32,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM,
+    });
+    const radiusBuffer = gpu.createBuffer({
+        size: sizes.f32,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM,
+    });
+    const uniformsLayout = gpu.createBindGroupLayout({
+        entries: [
+            { visibility, binding: 0, buffer: { type: "uniform" } },
+            { visibility, binding: 1, buffer: { type: "uniform" } },
+            { visibility, binding: 2, buffer: { type: "uniform" } },
+            { visibility, binding: 3, buffer: { type: "uniform" } },
+            { visibility, binding: 4, buffer: { type: "uniform" } },
+            { visibility, binding: 5, buffer: { type: "uniform" } },
+        ],
+    });
+    const uniformsBuffersBindGroup = gpu.createBindGroup({
+        layout: uniformsLayout,
+        entries: [
+            { binding: 0, resource: { buffer: rezBuffer } },
+            { binding: 1, resource: { buffer: timeBuffer } },
+            { binding: 2, resource: { buffer: countBuffer } },
+            { binding: 3, resource: { buffer: alphaBuffer } },
+            { binding: 4, resource: { buffer: betaBuffer } },
+            { binding: 5, resource: { buffer: radiusBuffer } },
+        ],
+    });
+    const writeUniforms = () => {
+        gpu.queue.writeBuffer(rezBuffer, 0, new Float32Array([uniforms.rez]));
+        gpu.queue.writeBuffer(countBuffer, 0, new Uint32Array([uniforms.count]));
+        gpu.queue.writeBuffer(alphaBuffer, 0, new Float32Array([(uniforms.alpha * Math.PI) / 180.0]));
+        gpu.queue.writeBuffer(betaBuffer, 0, new Float32Array([(uniforms.beta * Math.PI) / 180.0]));
+        gpu.queue.writeBuffer(radiusBuffer, 0, new Float32Array([uniforms.radius]));
+        settings.agentWorkgroups = Math.ceil(uniforms.count / 256);
+    };
+    writeUniforms();
+    // Other buffers
+    const positionsBuffer = gpu.createBuffer({
+        size: sizes.vec2 * uniforms.count,
+        usage: GPUBufferUsage.STORAGE,
+    });
+    const anglesBuffer = gpu.createBuffer({
+        size: sizes.f32 * uniforms.count,
+        usage: GPUBufferUsage.STORAGE,
+    });
+    const agentsLayout = gpu.createBindGroupLayout({
+        entries: [
+            { visibility, binding: 0, buffer: { type: "storage" } },
+            { visibility, binding: 1, buffer: { type: "storage" } },
+        ],
+    });
+    const agentsBuffersBindGroup = gpu.createBindGroup({
+        layout: agentsLayout,
+        entries: [
+            { binding: 0, resource: { buffer: positionsBuffer } },
+            { binding: 1, resource: { buffer: anglesBuffer } },
+        ],
+    });
+    /////
+    // Overall memory layout
+    const layout = gpu.createPipelineLayout({
+        bindGroupLayouts: [pixelBufferLayout, uniformsLayout, agentsLayout],
+    });
+    /////////////////////////
+    // Set up code instructions
+    const module = await createShaderModule(gpu, "../../src/pps/pps.wgsl");
+    const resetPipeline = gpu.createComputePipeline({
+        layout,
+        compute: { module, entryPoint: "reset" },
+    });
+    const simulatePipeline = gpu.createComputePipeline({
+        layout,
+        compute: { module, entryPoint: "simulate" },
+    });
+    const fadePipeline = gpu.createComputePipeline({
+        layout,
+        compute: { module, entryPoint: "fade" },
+    });
+    /////////////////////////
+    // RUN the reset shader function
+    const reset = () => {
+        const encoder = gpu.createCommandEncoder();
+        const pass = encoder.beginComputePass();
+        pass.setPipeline(resetPipeline);
+        pass.setBindGroup(0, pixelBufferBindGroup);
+        pass.setBindGroup(1, uniformsBuffersBindGroup);
+        pass.setBindGroup(2, agentsBuffersBindGroup);
+        pass.dispatchWorkgroups(settings.agentWorkgroups);
+        pass.end();
+        gpu.queue.submit([encoder.finish()]);
+    };
+    reset();
+    /////////////////////////
+    // RUN the sim compute function and render pixels
+    const draw = () => {
+        // Compute sim function
+        const encoder = gpu.createCommandEncoder();
+        const pass = encoder.beginComputePass();
+        pass.setBindGroup(0, pixelBufferBindGroup);
+        pass.setBindGroup(1, uniformsBuffersBindGroup);
+        pass.setBindGroup(2, agentsBuffersBindGroup);
+        pass.setPipeline(fadePipeline);
+        pass.dispatchWorkgroups(settings.pixelWorkgroups);
+        pass.setPipeline(simulatePipeline);
+        pass.dispatchWorkgroups(settings.agentWorkgroups);
+        pass.end();
+        // Render the pixels buffer to the canvas
+        render(gpu, uniforms.rez, pixelBuffer, format, context, encoder);
+        gpu.queue.submit([encoder.finish()]);
+        gpu.queue.writeBuffer(timeBuffer, 0, new Float32Array([uniforms.time++]));
+        setTimeout(draw, 10);
+    };
+    draw();
+    let container = document.getElementById("guiContainer");
+    if (!container) {
+        console.log("No GUI container found");
+        return;
+    }
+    let gui = new GUI({ container: container });
+    gui.add(uniforms, "alpha").min(-180).max(180);
+    gui.add(uniforms, "beta").min(-60).max(60);
+    gui.add(uniforms, "radius").min(0.0).max(64);
+    gui.add(uniforms, "count").min(1).max(12666).step(1);
+    gui.onChange(writeUniforms);
+}
+main();
---
+++ src/pps/pps.wgsl
@@ -0,0 +1,159 @@
+// Pixels
+@group(0) @binding(0)  
+  var<storage, read_write> pixels : array<vec4f>;
+
+// Uniforms
+@group(1) @binding(0)  
+  var<uniform> rez : f32;
+
+@group(1) @binding(1) 
+  var<uniform> time : f32;
+
+@group(1) @binding(2)  
+  var<uniform> count : u32;
+
+@group(1) @binding(3)  
+  var<uniform> alpha : f32;
+
+@group(1) @binding(4)  
+  var<uniform> beta : f32;
+
+@group(1) @binding(5)  
+  var<uniform> radius : f32;
+
+
+// Other buffers
+@group(2) @binding(0)  
+  var<storage, read_write> positions : array<vec2f>;
+
+@group(2) @binding(1)  
+  var<storage, read_write> angles : array<f32>;
+
+
+fn r(n: f32) -> f32 {
+  let x = sin(n) * 43758.5453;
+  return fract(x);
+}
+
+fn index(p: vec2f) -> i32 {
+  return i32(p.x) + i32(p.y) * i32(rez);
+}
+
+@compute @workgroup_size(256)
+fn reset(@builtin(global_invocation_id) id : vec3u) {
+  let seed = f32(id.x)/f32(count);
+  var p = vec2(r(seed), r(seed + 0.1));
+  p *= rez;
+  positions[id.x] = p;
+
+  angles[id.x] = r(f32(id.x) / 100) * 3.14 * 2;
+}
+
+
+fn hsv2rgb(h : f32, s: f32, v: f32) -> vec3f
+// Converts a color from the HSV (Hue, Saturation, Value) color space to the RGB (Red, Green, Blue) color space.
+// See https://en.wikipedia.org/wiki/HSL_and_HSV for more information, including pseudocode for this algorithm.
+{
+  var c = vec3(h, s, v);
+  var K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  var p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, vec3(0.0), vec3(1.0)), c.y);
+}
+
+fn isOnRightSide(p : vec2f, v : vec2f, op : vec2f) -> bool {
+  var b = p + v;
+  return ((b.x - p.x) * (op.y - p.y) - (b.y - p.y) * (op.x - p.x)) > 0;
+  /*
+  Let's break down the mathematics used in the code:
+
+  var b = p + v;: This line calculates the end point of the line segment by adding the vector v to the starting point p. This gives us the coordinates of the point b.
+
+  ((b.x - p.x) * (op.y - p.y) - (b.y - p.y) * (op.x - p.x)): This expression represents the cross product between two vectors. It can be written as:
+
+  (b.x - p.x) * (op.y - p.y) - (b.y - p.y) * (op.x - p.x)
+
+  Here's how this expression is computed:
+
+  (b.x - p.x) and (b.y - p.y) represent the components of the vector from p to b.
+  (op.x - p.x) and (op.y - p.y) represent the components of the vector from p to op.
+  By calculating the cross product of these two vectors, we can determine the relative positions of op and the line segment.
+
+  ((b.x - p.x) * (op.y - p.y) - (b.y - p.y) * (op.x - p.x)) > 0: This part of the code checks if the result of the cross product is greater than zero. 
+  If the result is positive, it means that op is on the right side of the line segment defined by p and b. 
+  If the result is zero or negative, it means that op is on the left side or collinear with the line segment.
+
+  Therefore, the isOnRightSide function uses the cross product of vectors to determine whether a point is on the right side of a line segment.
+
+  For two-dimensional vectors, the cross product is computed as follows:
+
+  Given two vectors:
+
+  Vector A: A = (A_x, A_y)
+  Vector B: B = (B_x, B_y)
+  The cross product of A and B is defined as:
+
+  A x B = A_x * B_y - A_y * B_x
+
+  The result of the cross product is a scalar value (not a vector) in two dimensions.
+
+  The sign of the resulting scalar indicates the orientation of the vectors. If the result is positive, it means that vector B need to move counterclockwise to align with vector A (andn lays on the 'right side' of A).
+  If the result is negative, it means that vector B needs to move clockwise to align with vector A (and lays on the 'left side' of A).
+  If the result is zero, it means that the vectors are collinear (lie on the same line).
+  */
+
+}
+
+@compute @workgroup_size(256)
+fn simulate(@builtin(global_invocation_id) id : vec3u) {
+  var p = positions[id.x];
+  var a = angles[id.x];
+  var v = vec2(cos(a), sin(a));
+
+  // PPS delta_phi algorithm
+  var l = 0.0;
+  var r = 0.0;
+  // For each other agent
+  for (var i = 0u; i < count; i++) {
+    if (i == id.x) { continue; } // ignore self
+    var op = positions[i];  // other position
+
+    if (distance(p, op) < radius) {
+      if (isOnRightSide(p, v, op)) {
+        r += 1.0;
+      } else {
+        l += 1.0;
+      }
+    }
+  }
+  var n = r + l;
+  var delta_phi = alpha + beta * n * sign(r - l);
+
+  // Rotate
+  a += delta_phi;
+
+  // Move
+  p += v;
+  p = (p + rez) % rez;
+
+  // Write
+  positions[id.x] = p;
+  angles[id.x] = a;
+
+  let sizei = i32(3);
+  for(var x = -sizei; x<= sizei; x++) {
+    for(var y=-sizei; y<= sizei; y++) {
+      let l = (length(vec2(f32(x), f32(y))));
+      if(l < f32(sizei)) {
+        pixels[index(p+vec2(f32(x), f32(y)))] += .10 * ( vec4(hsv2rgb(n/36, 0.9, 1.0), 1.0) * (1.0-l/f32(sizei)));
+      }
+    }
+  }
+
+  // Draw
+  //pixels[index(p)] = vec4(hsv2rgb(n/36, 1.0, 1.0), 1.0);
+}
+
+@compute @workgroup_size(256)
+fn fade(@builtin(global_invocation_id) id : vec3u) {
+  pixels[id.x] *= 0.90;
+}
