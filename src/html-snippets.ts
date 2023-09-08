0f1a25d81955257d4b6e6b4b13a4b07ef127e0e1
craftlinks <90352073+craftlinks@users.noreply.github.com>
2023-09-08 15:46:55 +0200

    prep for NN

--- src/html-snippets.ts
+++ src/html-snippets.ts
@@ -22,5 +22,6 @@ function navigationButtons (): string {
     <a href = "${baseURL}/pages/predator-prey.html" class="button">Predator-prey</a>
     <a href = "${baseURL}/pages/pps.html" class="button">Primordial Particle System</a>
     <a href = "${baseURL}/pages/particle-life.html" class="button">Particle Life</a>
-    <a href = "${baseURL}/pages/neurons.html" class="button">Neurons</a>`
+    <a href = "${baseURL}/pages/bits.html" class="button">Bits</a>
+    <a href = "${baseURL}/pages/webgl.html" class="button">GL</a>`
 }
